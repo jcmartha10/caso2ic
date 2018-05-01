f6ec7d7e139c82a0a5421e2f92052ac08c7016f7
Juank <jc.martha10@uniandes.edu.co>
2018-05-01 15:43:21 -0500

    Caso 2 Full (Revisar MD5)

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,5 +1,6 @@
 package logica;
 
+import java.awt.FontFormatException;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
@@ -22,22 +24,29 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import javax.security.auth.x500.X500Principal;
+import javax.xml.bind.DatatypeConverter;
 
-import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
-import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
-import org.bouncycastle.x509.extension.AuthorityKeyIdentifierStructure;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 
-@SuppressWarnings("deprecation")
 public class Cliente {
 	private final String HOLA = "HOLA";
 	private final String INICIO = "INICIO";
@@ -49,19 +58,19 @@ public class Cliente {
 	private final String CERTSRV = "CERTSRV";
 	private final String ACT1 = "ACT1";
 	private final String ACT2 = "ACT2";
-	private final String[] ALGS  = {"RSA"};
+	private final String[] ALGS  = {"AES", "RSA", "HMACMD5"};
 	
 	private String posicion;
-	private String posicion2;
 	private Socket socket;
 	PrintWriter escritor;
 	BufferedReader lector;
+	private PrivateKey llavePrivada;
+	private X509Certificate certificadoServidor;
 	
-	public Cliente() {
+	public Cliente() throws UnknownHostException, IOException {
 		posicion = "41 24.2028, 2 10.4418";
-		posicion2 = "42 24.2028, 3 10.4418";
-		socket = new Socket();
-		
+		socket = new Socket("localhost", 4321);
+
 		try {
 			escritor = new PrintWriter(socket.getOutputStream(), true);
 			lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -70,7 +79,7 @@ public class Cliente {
 	
 	public void ejecutar() throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IllegalStateException, SignatureException, NoSuchProviderException, CertificateException {
 		String respuesta = "";
-		escritor.print(HOLA);
+		escritor.println(HOLA);
 		
 		respuesta = lector.readLine();
 		
@@ -79,6 +88,7 @@ public class Cliente {
 			for (int i = 0; i < ALGS.length; i++) {
 				alg += ":" + ALGS[i];
 			}
+			escritor.println(alg);
 		} else {
 			System.out.println("ERROR: Mensaje erróneo del servidor: " + respuesta);
 			return;
@@ -104,23 +114,19 @@ public class Cliente {
 		
 		respuesta = lector.readLine();
 		if (respuesta.equals(ESTADO + ":" + OK)) {
+
 			respuesta = lector.readLine();
 			byte[] certificadoSrv = new byte[tamanioBytes];
 			
 			if (respuesta.equals(CERTSRV)) {
-				socket.getInputStream().read(certificadoSrv);
 				CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-				InputStream in = new ByteArrayInputStream(certificadoSrv);
-				X509Certificate certSrv = (X509Certificate)certFactory.generateCertificate(in);
-				
-				if (certClt.equals(certSrv)) {
-					escritor.println(ESTADO + ":" + OK);
-				} else {
-					escritor.println(ESTADO + ":" + ERROR);
-					System.out.println("Error: El certificado no es valido.");
-					return;
-				}
-				
+	            byte[] certificadoClienteBytes = new byte[5000]; // buffer para almacenar los bytes del certificado (en cliente, este tiene un un tamanio de mas o menos467 bytes)
+	            
+	            socket.getInputStream().read(certificadoClienteBytes);
+	            InputStream inputStream = new ByteArrayInputStream(certificadoClienteBytes);
+            	certificadoServidor = (X509Certificate)certFactory.generateCertificate(inputStream);
+            	
+            	escritor.println(ESTADO + ":" + OK);
 			}
 		} else if (respuesta.equals(ESTADO + ":" + ERROR)) {
 			System.out.println("ERROR: El servidor envió ERROR como respuesta.");
@@ -130,26 +136,59 @@ public class Cliente {
 			return;
 		}
 		
+		respuesta = "";
 		respuesta = lector.readLine();
-		if (respuesta.split(":")[0].equals(INICIO)) {
-			byte[] l = new byte[256];
-			socket.getInputStream().read(l);
-			SecretKeySpec skc = new SecretKeySpec(l, "AES");
-			Cipher ca = Cipher.getInstance("AES/CBC/PKCS7Padding");
-			ca.init(Cipher.DECRYPT_MODE, skc);
-			byte[] decryptedBytes = ca.doFinal(l);
-			//String decryptedString = new String(decryptedBytes);
-			Key key = new SecretKeySpec(decryptedBytes, 0, decryptedBytes.length, "AES");
+		
+		String[] resp = respuesta.split(":");
+		System.out.println(respuesta);
+		System.out.println(resp[0]);
+		if (resp[0].equals(INICIO)) {
+			String ll = resp[1];
+			System.out.println("01");
+			Cipher decifrador = Cipher.getInstance(ALGS[1]);
+			System.out.println("02");
+			decifrador.init(Cipher.DECRYPT_MODE, llavePrivada);
+			System.out.println("03");
+			byte[] c = DatatypeConverter.parseHexBinary(ll);
+			System.out.println("04");
+			byte[] descifrado = decifrador.doFinal(c);
+			System.out.println("1");
+			SecretKey key = new SecretKeySpec(descifrado, 0, descifrado.length, ALGS[0]); //Obtenemos la llave secreta
 			
-			ca.init(Cipher.ENCRYPT_MODE, key);
-			byte[] posEncriptado = ca.doFinal(posicion.getBytes());
+			
+			decifrador=Cipher.getInstance(ALGS[0]);
+			System.out.println("8");
+			decifrador.init(Cipher.ENCRYPT_MODE, key);
+			System.out.println("9");
+			byte[] posEncriptado = decifrador.doFinal(posicion.getBytes()); //Encriptamos la posicion con la llave secreta
+			System.out.println("10");
+			
+			/*Mac mac = Mac.getInstance(ALGS[2]);
+			mac.init(key);
+			byte[] posHash = DatatypeConverter.parseHexBinary(posicion);
+			byte[] elHash = mac.doFinal(posHash);*/
 			
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			md5.update(posicion.getBytes());
-			byte[] elHash = md5.digest();
+			//md5.update(posicion.getBytes());
+			byte[] elHash = md5.digest(posicion.getBytes());
 			
-			escritor.println(ACT1 + ":" + posEncriptado);
-			escritor.println(ACT2 + ":" + elHash);
+			System.out.println("12");
+			//byte[] elHash = md5.digest();
+			System.out.println("13");
+			System.out.println(posEncriptado);
+			decifrador=Cipher.getInstance(ALGS[1]);
+			System.out.println("8");
+			decifrador.init(Cipher.ENCRYPT_MODE, certificadoServidor.getPublicKey());
+			System.out.println("9");
+			byte[] posHashEncriptado = decifrador.doFinal(elHash);
+			System.out.println("10");
+			System.out.println("Hash md5 del cliente: " + DatatypeConverter.printHexBinary(elHash));
+			
+			escritor.println(ACT1 + ":" + DatatypeConverter.printHexBinary(posEncriptado));
+			System.out.println("14");
+			//System.out.println(DatatypeConverter.printHexBinary(posHashEncriptado));
+			escritor.println(ACT2 + ":" + DatatypeConverter.printHexBinary(posHashEncriptado));
+			System.out.println("15");
 		}
 		
 		respuesta = lector.readLine();
@@ -166,33 +205,46 @@ public class Cliente {
 	}
 
 	private X509Certificate certificado() throws CertificateParsingException, CertificateEncodingException, InvalidKeyException, IllegalStateException, NoSuchAlgorithmException, SignatureException, NoSuchProviderException {
-		Date startDate = new Date();                // time from which certificate is valid
-		Date expiryDate = new Date();               // time after which certificate is not valid
-		BigInteger serialNumber = BigInteger.TEN;       // serial number for certificate
+		String stringDate1 = "2016-10-01";
+	    String stringDate2 = "2020-12-20";
+	    DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	    Date startDate = null;
+	    Date expiryDate = null;
+	    try
+	    {
+	    	startDate = format.parse(stringDate1);
+	    	expiryDate = format.parse(stringDate2);
+	    }
+	    catch (Exception e) {}
+	    
+		BigInteger serialNumber = new BigInteger(128, new Random());       // serial number for certificate
+		
 		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-		generator.initialize(1024);
-		PrivateKey caKey = generator.genKeyPair().getPrivate();              // private key of the certifying authority (ca) certificate
-		X509Certificate caCert = new X509V3CertificateGenerator().generate(caKey);        // public key certificate of the certifying authority
-		KeyPair keyPair = generator.genKeyPair();               // public/private key pair that we are creating certificate for
-		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
-		X500Principal              subjectName = new X500Principal("CN=Test V3 Certificate");
-		 
-		certGen.setSerialNumber(serialNumber);
-		certGen.setIssuerDN(caCert.getSubjectX500Principal());
-		certGen.setNotBefore(startDate);
-		certGen.setNotAfter(expiryDate);
-		certGen.setSubjectDN(subjectName);
-		certGen.setPublicKey(keyPair.getPublic());
-		certGen.setSignatureAlgorithm("RSA");
-		 
-		certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false, new AuthorityKeyIdentifierStructure(caCert));
-		certGen.addExtension(X509Extensions.SubjectKeyIdentifier, false, new SubjectKeyIdentifier(keyPair.getPublic().getEncoded() ));
-		 
-		X509Certificate cert = certGen.generate(caKey, "BC");   // note: private key of CA
-		return cert;
+		KeyPair keyPair = generator.genKeyPair();   
+		this.llavePrivada = keyPair.getPrivate();
+		
+		 X500NameBuilder nameBuilder = new X500NameBuilder(BCStyle.INSTANCE);
+		    nameBuilder.addRDN(BCStyle.OU, "OU");
+		    nameBuilder.addRDN(BCStyle.O, "O");
+		    nameBuilder.addRDN(BCStyle.CN, "CN");
+		    
+		X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder(nameBuilder.build(), serialNumber, startDate, expiryDate, nameBuilder.build(), keyPair.getPublic());
+	    X509Certificate certificate = null;
+	    try
+	    {
+	      ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(keyPair.getPrivate());
+	      certificate = new JcaX509CertificateConverter().getCertificate(certificateBuilder.build(contentSigner));
+	    }
+	    catch (Exception e)
+	    {}
+	    
+	    return certificate;
 	}
 	
 	public static void main(String[] args) {
-		
+		try {
+			Cliente c = new Cliente();
+			c.ejecutar();
+		} catch (Exception e) {e.printStackTrace();}
 	}
 }
